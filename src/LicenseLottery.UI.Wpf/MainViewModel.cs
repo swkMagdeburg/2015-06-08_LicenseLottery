7c432f87c3634ded880a10c43e39e5ad44ef8e03
Andreas Richter <ar@anrichter.net>
2015-05-31 22:42:14 +0200

    Design the GUI

--- src/LicenseLottery.UI.Wpf/MainViewModel.cs
+++ src/LicenseLottery.UI.Wpf/MainViewModel.cs
@@ -1,6 +1,44 @@
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using LicenseLottery.Core.Entities;
+
 namespace LicenseLottery.UI.Wpf
 {
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        private Lottery _createdLottery;
+        private string _newLotteryName;
+
+        public MainViewModel()
+        {
+            CreateNewLottery = new RelayCommand(CreateNewLotteryExecute, CreateNewLotteryCanExecute);
+        }
+
+        public RelayCommand CreateNewLottery { get; private set; }
+
+        public string NewLotteryName
+        {
+            get { return _newLotteryName; }
+            set { Set(() => NewLotteryName, ref _newLotteryName, value); }
+        }
+
+        public Lottery CreatedLottery
+        {
+            get { return _createdLottery; }
+            set
+            {
+                Set(() => CreatedLottery, ref _createdLottery, value);
+                CreateNewLottery.RaiseCanExecuteChanged();
+            }
+        }
+
+        private void CreateNewLotteryExecute()
+        {
+        }
+
+        private bool CreateNewLotteryCanExecute()
+        {
+            return !string.IsNullOrWhiteSpace(_newLotteryName);
+        }
     }
 }
