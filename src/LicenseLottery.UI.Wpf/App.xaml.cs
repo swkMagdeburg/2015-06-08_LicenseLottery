c89d31626d37e7e082b844d382d132b7efc5d67d
Andreas Richter <ar@anrichter.net>
2015-05-31 18:49:53 +0200

    Prepare the UI for MVVM

--- src/LicenseLottery.UI.Wpf/App.xaml.cs
+++ src/LicenseLottery.UI.Wpf/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace LicenseLottery.UI.Wpf
 {
@@ -13,5 +7,11 @@ namespace LicenseLottery.UI.Wpf
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainViewModel = new MainViewModel();
+            var view = new MainWindow { DataContext = mainViewModel };
+            view.Show();
+        }
     }
 }
