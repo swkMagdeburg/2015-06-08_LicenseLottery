f0bb5f359995a3f1d03e7bbf9e203044222fd49d
Andreas Richter <ar@anrichter.net>
2015-05-31 16:18:27 +0200

    Implement the core functionality

---
+++ src/LicenseLottery.Core/UseCases/Implementations/ILotteryRepository.cs
@@ -0,0 +1,9 @@
+﻿using LicenseLottery.Core.Entities;
+
+namespace LicenseLottery.Core.UseCases.Implementations
+{
+    internal interface ILotteryRepository
+    {
+        void Add(Lottery lottery);
+    }
+}
