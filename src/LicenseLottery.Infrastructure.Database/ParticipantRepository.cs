01caa4987c0637b1d0b0d22fe1c2d706854efd30
Andreas Richter <ar@anrichter.net>
2015-06-03 20:01:53 +0200

    Implement an in Memory ParticipantRepository

---
+++ src/LicenseLottery.Infrastructure.Database/ParticipantRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using LicenseLottery.Core.Entities;
+using LicenseLottery.Core.UseCases.Implementations;
+
+namespace LicenseLottery.Infrastructure.Database
+{
+    public class ParticipantRepository : IParticipantRepository
+    {
+        private static readonly HashSet<Participant> Participants = new HashSet<Participant>();
+
+        public void Add(Participant participant)
+        {
+            Participants.Add(participant);
+        }
+
+        public IEnumerable<Participant> All()
+        {
+            return Participants;
+        }
+    }
+}
