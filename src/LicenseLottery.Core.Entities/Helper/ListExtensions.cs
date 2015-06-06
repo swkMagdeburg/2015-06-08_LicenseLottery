a2109291b775fe6b57af25675dd6bc68deb474d1
Andreas Richter <ar@anrichter.net>
2015-06-06 17:15:13 +0200

    Use a better solution for randomize a List

--- src/LicenseLottery.Core.Entities/Helper/ListExtensions.cs
+++ src/LicenseLottery.Core.Entities/Helper/ListExtensions.cs
@@ -1,23 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace LicenseLottery.Core.Entities.Helper
 {
     internal static class ListExtensions
     {
+        /// <summary>
+        /// Shuffle found on http://stackoverflow.com/questions/273313/randomize-a-listt-in-c-sharp
+        /// </summary>
         public static void Shuffle<T>(this IList<T> list)
         {
-            var random = new Random();
-            var n = list.Count;
-            while (n > 1)
+            var index = list.Count;
+            while (index > 1)
             {
-                n--;
-
-                var k = random.Next(n + 1);
-
-                var value = list[k];
-                list[k] = list[n];
-                list[n] = value;
+                index--;
+                var randomIndex = ThreadSafeRandom.ThisThreadsRandom.Next(index + 1);
+                
+                var value = list[randomIndex];
+                list[randomIndex] = list[index];
+                list[index] = value;
             }
         }
 
---
+++ src/LicenseLottery.Core.Entities/Helper/ThreadSafeRandom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace LicenseLottery.Core.Entities.Helper
+{
+    /// <summary>
+    /// ThreadSafeRandom found on http://stackoverflow.com/questions/273313/randomize-a-listt-in-c-sharp
+    /// </summary>
+    public static class ThreadSafeRandom
+    {
+        [ThreadStatic]
+        private static Random _local;
+
+        public static Random ThisThreadsRandom
+        {
+            get { return _local ?? (_local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
+        }
+    }
+}
